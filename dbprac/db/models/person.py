c06076343d7426a3e0542cfeda9918ca5fb677eb
vvhob <h2michalski@gmail.com>
2024-03-18 16:58:03 +0100

    person

--- dbprac/db/models/person.py
+++ dbprac/db/models/person.py
@@ -1,13 +1,12 @@
 from sqlalchemy import Column, Integer, String
 from sqlalchemy.orm import relationship
-from .base import Base
 
-# from sqlalchemy import Boolean, Date, DateTime, Time
+from .base import Base
 
 class Person(Base):
     __tablename__ = 'person'
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, index=True, autoincrement=True)
     f_name = Column(String)
     l_name = Column(String)
     sex = Column(String)
@@ -26,38 +25,3 @@ class Person(Base):
     def __str__(self):
         return (f'Person(id={self.id}, f_name={self.f_name}, l_name={self.l_name}, '
                 f'sex={self.sex}, email={self.email}, phone_num={self.phone_num})')
-
-'''
-    foreign key
-    foreign_id = Column(Integer, ForeignKey('other_table.id'))
-    
-    nullable
-    name = Column(String, nullable=False)
-
-    uniqueness
-    email = Column(String, unique=True)
-
-    czy zaloz index / indexed_column
-    indexed_column = Column(String, index=True)
-
-    default value
-    created_at = Column(DateTime, default=datetime.utcnow)
-
-    composite primary key
-    Column('first_name', String, primary_key=True)
-    Column('last_name', String, primary_key=True)
-
-    
-    #Relationship options
-
-    relationshipsy sa bardzo fajne, bo mozesz latwo pozniej dostac sie do relateowany rekordow, poprzez
-    ta wlasnosc po prostu, bez zadnego joinowania itp. takze to uzywac tego
-
-    Relationship Options
-    children = relationship("Child", back_populates="parent")
-    parent = relationship("Parent", back_populates="children")
-    children = relationship("Child", cascade="all, delete, delete-orphan")
-
-    #contraints
-    __table_args__ = (CheckConstraint('price >= 0', name='check_price_positive'),)
-'''
