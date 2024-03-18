5faa4805676e23c40696827bb3d5fc4e8adc10fd
vvhob <h2michalski@gmail.com>
2024-03-18 18:08:00 +0100

    generator make_employee() changes

--- dbprac/generator.py
+++ dbprac/generator.py
@@ -1,10 +1,8 @@
 import random
 from faker import Faker
-from datetime import datetime, timedelta
 from unidecode import unidecode
 
-from db.models import Person
-from db.models import Employee
+from db.models import *
 
 class Generator():
     def __init__(self):
@@ -41,12 +39,47 @@ class Generator():
         return Person(f_name, l_name, sex, phone_num, email)
     
     def make_employee(self, person: Person) -> Employee:
-        return
+        date_of_birth = self.fake.date_of_birth(minimum_age=18) # type = <class 'datetime.date'>
+        pesel = self.fake.pesel(date_of_birth=date_of_birth, sex=person.sex)
+        
+        # random employment and leave date, leave date can be null, construtor takes in 'date' from datetime 
+        # get_position_list, random position, a departament chyba trza bedzie zrobic assign albo tak samo ze na sztywno
+        # liste departamentow
+
+        return Employee(pesel, )
 
 gen = Generator()
 for _ in range(0, 101):
-    print(gen.random_person())
+    p = gen.random_person()
+    gen.make_employee(p)
 
 # instance.__dict__ -> object's writable attributes, does not include methods or attributes inherited 
 # dir(instance) -> object's attributes and methods (everything)
 # inspect.getsource(obj) -> return the text of the source code for an object. inspect module 
+    
+# birth_date_time = self.fake.date_time_between(datetime(1945, 5, 8), datetime.now() - timedelta(days=18*365))        
+    
+'''
+popular_car_makes = [
+    "Toyota",
+    "Volkswagen",
+    "Ford",
+    "Honda",
+    "Chevrolet",
+    "Mercedes-Benz",
+    "BMW",
+    "Tesla",
+    "Audi",
+    "Hyundai",
+    "Nissan",
+    "Kia",
+    "Porsche",
+    "Subaru",
+    "Lexus",
+    "Mazda",
+    "Dodge",
+    "Peugeot",
+    "Jeep",
+    "Renault"
+]
+'''
